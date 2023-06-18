064d812fb5d6ca663f7d3cdf987affdfe651b9d3
Mira Miteva <mira.miteva92@gmail.com>
2023-06-18 14:26:55 +0300

    add use session hook

---
+++ src/hooks/useSession.ts
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+
+const useSession = () => {
+    const [alreadyFocused, setAlreadyFocused] = useState(false);
+    const [activeTime, setActiveTime] = useState(0);
+    let lastActiveTime = Date.now();
+
+    useEffect(() => {
+        const windowFocusEventHandler = () => {
+            if (!alreadyFocused) {
+                lastActiveTime = Date.now();
+                console.log('windowFocusEventHandler');
+                setAlreadyFocused(true);
+            }
+        }
+        const windowBlurEventHandler = () => {
+            if (!document.hidden) return;
+            const elapsedSeconds = Math.floor((Date.now() - lastActiveTime) / 1000);
+            setActiveTime(activeTime => activeTime + elapsedSeconds);
+            console.log('windowBlurEventHandler');
+            setAlreadyFocused(false);
+            localStorage.setItem('activeTime', elapsedSeconds.toString());
+        }
+
+        window.addEventListener('focus', windowFocusEventHandler, true);
+        window.addEventListener('visibilitychange', windowBlurEventHandler, true);
+
+        return () => {
+            window.removeEventListener('focus', windowFocusEventHandler);
+            window.removeEventListener('visibilitychange', windowBlurEventHandler);
+        }
+    }, []);
+}
+
+export default useSession;
