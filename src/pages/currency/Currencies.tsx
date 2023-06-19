d8771a7cfbee671f1f09e5088e91fb19f33de5d3
Radoslav Todorov <radoslav.emilov.todorov@gmail.com>
2023-06-19 08:17:34 +0300

    Currencies - restrictions for saving item without name or abbreviation or conversationRateToBGN, conversationRateToBGN - positive number

--- src/pages/currency/Currencies.tsx
+++ src/pages/currency/Currencies.tsx
@@ -172,12 +172,7 @@ export default function Currencies() {
   const processRowUpdate = async (newRow: GridRowModel) => {
     const currentRow = rows?.find((row) => row.id === newRow.id);
     
-    toast(currentRow?.actionTypeApplied === Enums.ActionType.CANCELED 
-        ? 'Canceled' 
-        : `Saved currency with id ${currentRow!.id}`,
-      {
-        position: 'top-center',
-      });
+    
     if(currentRow?.actionTypeApplied === Enums.ActionType.SAVED
         || currentRow?.actionTypeApplied === Enums.ActionType.EDITED) {
           const newRowData: CurrencyModel = {
@@ -192,6 +187,30 @@ export default function Currencies() {
               ? new Date()
               : newRow.dateChanged
           };
+
+          if(!newRowData.name
+            || !newRowData.abbreviation
+            || newRowData.conversionRateToBGN === null) {
+            setRowModesModel((previousRowModesModel) => {
+              return { ...previousRowModesModel, [currentRow.id!.toString()]: { mode: GridRowModes.Edit } }
+            });
+            toast(`Name, abbreviation and counversations rate to bgn should be specified!`, {
+              position: 'top-center',
+            });
+            
+            return;
+          }
+
+          if(newRowData.conversionRateToBGN < 0) {
+            setRowModesModel((previousRowModesModel) => {
+              return { ...previousRowModesModel, [currentRow.id!.toString()]: { mode: GridRowModes.Edit } }
+            });
+            toast(`Counversation rate to bgn should be 0 or greater!`, {
+              position: 'top-center',
+            });
+            
+            return;
+          }
 
           setIsLoading(true);
           
@@ -211,13 +230,20 @@ export default function Currencies() {
             });
           });
 
+          setRowModesModel((previousRowModesModel) => {
+            return { ...previousRowModesModel, [newRow.id]: { mode: GridRowModes.View } }
+          });
+
           setIsLoading(false);
     } else {
 
     }
     
-    setRowModesModel((previousRowModesModel) => {
-      return { ...previousRowModesModel, [newRow.id]: { mode: GridRowModes.View } }
+    toast(currentRow?.actionTypeApplied === Enums.ActionType.CANCELED 
+      ? 'Canceled' 
+      : `Saved currency with id ${currentRow!.id}`,
+    {
+      position: 'top-center',
     });
 
     return newRow;
