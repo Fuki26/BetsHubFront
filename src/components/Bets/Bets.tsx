c66d01f87a81892269f2c2d223ad9c5fb26de8e6
AnzheloStoyanov <anjelostoqnov@gmail.com>
2023-07-16 12:47:19 +0300

    The cells in pednig are editable now

--- src/components/Bets/Bets.tsx
+++ src/components/Bets/Bets.tsx
@@ -12,6 +12,7 @@ import {
   GridRowModes,
   GridRowModesModel,
   GridRowParams,
+  GridCellParams,
   GridToolbarContainer,
   GridValueGetterParams,
 } from "@mui/x-data-grid";
@@ -42,6 +43,7 @@ import { deleteBet, upsertBet, getBetHistory } from "../../api";
 import { BetStatus, WinStatus, LiveStatus } from "../../models/enums";
 import { Currency } from "../../database-models";
 import Modal from "../UI/Modal";
+import { ConstructionOutlined } from "@mui/icons-material";
 
 const getAbbreviations = (currencies: Currency[] | undefined) => {
   if (!currencies) return [];
@@ -381,25 +383,40 @@ function Bets(props: {
     const row = rows.find((row) => row.id === params.id);
     if (row) {
       selectBetIdFn(row.id);
+     
+
     }
   };
+  const onCellEditStop = (params: GridCellParams) => {
+    const row = rows.find((row) => row.id === params.id);
+    if (row) {
+      handleEditClick(row.id)()
+    
+    }
+  };
+  
+  
+
 
   //#endregion Actions handlers
 
   //#region Rows update handler
 
   const processRowUpdate = async (
+    
     newRow: GridRowModel<BetModel>
   ): Promise<BetModel> => {
     const currentRow = rows.find((row) => row.id === newRow.id);
     if (!currentRow) {
       return newRow;
+      
     }
 
     if (
       currentRow.actionTypeApplied === Enums.ActionType.SAVED ||
-      currentRow.actionTypeApplied === Enums.ActionType.EDITED
+      currentRow.actionTypeApplied === Enums.ActionType.EDITED 
     ) {
+      
       const amounts = Object.fromEntries(Object.entries(newRow).filter(([key, value]) => key.startsWith('amount')));
       const newRowData: BetModel = {
         ...currentRow,
@@ -423,10 +440,12 @@ function Bets(props: {
 
         selection: newRow.selection,
       };
+      
       setIsLoading(true);
 
       const rowData = await upsertBet(newRowData);
       if (!rowData || !rowData.data) {
+        console.log(newRow)
         return newRow;
       }
 
@@ -443,6 +462,7 @@ function Bets(props: {
             };
           } else {
             return row;
+            
           }
         });
       });
@@ -458,6 +478,7 @@ function Bets(props: {
 
       newRow.id = rowData?.data.id;
     } else {
+      
       setRowModesModel((previousRowModesModel) => {
         return {
           ...previousRowModesModel,
@@ -474,7 +495,6 @@ function Bets(props: {
         position: "top-center",
       }
     );
-
     return newRow;
   };
 
@@ -1184,13 +1204,17 @@ function Bets(props: {
             slots={{
               toolbar: isRead ? undefined : EditToolbar,
             }}
+            
             rowModesModel={rowModesModel}
             processRowUpdate={processRowUpdate}
             slotProps={{
               toolbar: { setRows, setRowModesModel },
             }}
             onRowClick={onRowClick}
-            editMode="row"
+      
+            onCellEditStop={onCellEditStop}
+            // editMode="row"
+            editMode="cell"
             sx={{
               height: 500,
             }}
