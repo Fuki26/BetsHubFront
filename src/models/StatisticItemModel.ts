f261e3db77f01c91ba47ec141f8659d7105b71ea
Stefan Todorov <stodorov988@gmail.com>
2023-08-28 18:11:30 +0300

    added percentage for statistics

--- src/models/StatisticItemModel.ts
+++ src/models/StatisticItemModel.ts
@@ -1,8 +1,8 @@
 export type StatisticItemModel = {
     id: number,
     periodType: 'today' | 'last 3m' | 'last 6m',
-    winRate: number;
-    yield: number;
+    winRate: string;
+    yield: string;
     turnOver: number;
     profit: number;
 };
