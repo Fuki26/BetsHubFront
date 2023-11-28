0d1fb6d2660bc1af0eff7ca19faff82407ab3a59
Radoslav Todorov <radoslav.emilov.todorov@gmail.com>
2023-11-28 09:19:10 +0200

    https://trello.com/c/ytMPuvng - FIxed

--- src/theme/create-palette.ts
+++ src/theme/create-palette.ts
@@ -13,8 +13,8 @@ export function createPalette() {
       selected: alpha(neutral[900], 0.12)
     },
     background: {
-      default: common.white,
-      paper: common.white
+      default: '#d6cbd3',
+      paper: '#d6cbd3'
     },
     divider: '#F2F4F7',
     error,
